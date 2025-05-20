50ae05a79444e376a9f235c0c769c2a99aae7327
CarloLaguda <carlo.laguda07@gmail.com>
2025-05-20 12:51:20 +0000

    punti 1 e 2

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+#inizializza l'app Flask
+app = Flask(__name__)
+
+lista_spesa = ["Pesca", "Mele", "Carne"]
+#rotta principale
+@app.route('/')
+def home():
+    return render_template('index.html', lista = lista_spesa)
+#avvio dell'app Flask
+if __name__ == '__main__':
+    app.run(debug=True)
